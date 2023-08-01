5b20cfadbaed3d63efae074400df9a358c465720
moon daehyun <cbmd97@naver.com>
2023-08-01 19:28:49 +0900

    ✨ #1 관리자 페이지 더미 api연동

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,120 +1,61 @@
-import React from "react";
+import { useEffect, useState } from 'react';
 import styled from "styled-components";
 import { Table, Button } from "antd";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { theme } from "../../styles/theme";
-
-const PageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 100vh;
-`;
-
-const PageLogoText = styled.h1`
-  font-size: 32px;
-  font-weight: bold;
-  color: ${theme.colors.primary};
-  margin-bottom: 50px;
-  margin-top: 70px;
-  margin-right: 800px;
-`;
-
-const Container = styled.div`
-  width: 70%;
-  display: flex;
-  justify-content: space-between;
-  position: relative;
-  margin-top: 60px;
-`;
-
-const TableWrapper = styled.div`
-  border: 1px solid #ccc; // 테이블을 감싸는 border 스타일
-  border-radius: 4px;
-  width: 600px;
-  padding: 30px;
-  border-radius: 10px;
-`;
-
-const Divider = styled.div`
-  width: 1px;
-  background-color: #ccc;
-  margin: 0 auto;
-`;
-
-const TableHeader = styled.h2`
-  margin-bottom: 20px;
-  margin-right: 280px;
-`;
-
-const StyledholidayApproveButton = styled(Button)`
-  background-color: ${theme.colors.white};
-  color: ${theme.colors.orange.main};
-  margin-right: 10px;
-
-  &:hover {
-    background-color: ${theme.colors.orange.main};
-    color: ${theme.colors.white};
-  }
-`;
-
-const StyledholidayRejectButton = styled(Button)`
-  background-color: ${theme.colors.orange.main};
-  color: ${theme.colors.white};
-
-  &:hover {
-    background-color: ${theme.colors.orange.dark};
-    color: ${theme.colors.white};
-  }
-`;
-
-const StyleddutyApproveButton = styled(Button)`
-  background-color: ${theme.colors.white};
-  color: ${theme.colors.green.main};
-  margin-right: 10px;
-
-  &:hover {
-    background-color: ${theme.colors.green.main};
-    color: ${theme.colors.white};
-  }
-`;
-
-const StyleddutyRejectButton = styled(Button)`
-  background-color: ${theme.colors.green.main};
-  color: ${theme.colors.white};
-
-  &:hover {
-    background-color: ${theme.colors.green.dark};
-    color: ${theme.colors.white};
-  }
-`;
+import axios from 'axios'; // axios를 추가합니다.
 
 const App = () => {
+  const [dataSource1, setDataSource1] = useState([]); // 연차 신청 데이터를 담을 state입니다.
+  const [dataSource2, setDataSource2] = useState([]); // 당직 신청 데이터를 담을 state입니다.
+
+  // API에서 데이터를 받아와서 dataSource1과 dataSource2에 저장합니다.
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('https://b79e656d-ef86-45fe-a5cb-a112eafd50a8.mock.pstmn.io/admin/event/request');
+      console.log(result.data);  // 추가: API에서 어떤 데이터가 오는지 확인하기 위한 console.log입니다.
+  
+      const leaveRequests = result.data.data.content.filter(request => request.eventType === 'leave');
+      const dutyRequests = result.data.data.content.filter(request => request.eventType === 'duty');
+  
+      setDataSource1(leaveRequests);
+      setDataSource2(dutyRequests);
+    }
+  
+    fetchData();
+  }, []);
+
+  // 연차 신청 현황 테이블
   const columns1 = [
     {
       title: "이름",
-      dataIndex: "name",
-      key: "name",
+      dataIndex: "userName",
+      key: "userName",
     },
     {
-      title: "날짜",
-      dataIndex: "date",
-      key: "date",
+      title: "시작일",
+      dataIndex: "startDate",
+      key: "startDate",
+    },
+    {
+      title: "종료일",
+      dataIndex: "endDate",
+      key: "endDate",
     },
     {
       title: "남은 연차",
-      dataIndex: "address",
-      key: "address",
+      dataIndex: "annualCount",
+      key: "annualCount",
     },
     {
       title: "승인/취소",
       key: "actions",
       render: (text, record) => (
-        <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
-          <StyledholidayApproveButton type="ghost" onClick={() => handleApprove(record)}>
+        <div style={{ display: "flex", justifyContent: "center", marginTop: "5px" }}>
+          <StyledLeaveApproveButton type="ghost" onClick={() => handleApprove(record)}>
             승인
-          </StyledholidayApproveButton>
+          </StyledLeaveApproveButton>
           <StyledholidayRejectButton type="ghost" onClick={() => handleReject(record)}>
             취소
           </StyledholidayRejectButton>
@@ -123,43 +64,28 @@ const App = () => {
     },
   ];
 
-  const dataSource1 = [
-    {
-      key: "1",
-      name: "John Doe",
-      date: 28,
-      address: "15",
-    },
-    {
-      key: "2",
-      name: "Jane Smith",
-      date: 32,
-      address: "14",
-    },
-    {
-      key: "3",
-      name: "Smith",
-      date: 42,
-      address: "11",
-    },
-  ];
-
+  // 당직 신청 현황 테이블
   const columns2 = [
     {
       title: "이름",
-      dataIndex: "name",
-      key: "name",
+      dataIndex: "userName",
+      key: "userName",
     },
     {
-      title: "날짜",
-      dataIndex: "date",
-      key: "date",
+      title: "시작일",
+      dataIndex: "startDate",
+      key: "startDate",
+    },
+    {
+      title: "종료일",
+      dataIndex: "endDate",
+      key: "endDate",
     },
     {
       title: "승인/취소",
       key: "actions",
       render: (record) => (
-        <div style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}>
+        <div style={{ display: "flex", justifyContent: "center", marginTop: "5px" }}>
           <StyleddutyApproveButton type="ghost" onClick={() => handleApprove(record)}>
             승인
           </StyleddutyApproveButton>
@@ -168,24 +94,6 @@ const App = () => {
           </StyleddutyRejectButton>
         </div>
       ),
-    },
-  ];
-
-  const dataSource2 = [
-    {
-      key: "1",
-      name: "Tom Hanks",
-      date: 23,
-    },
-    {
-      key: "2",
-      name: "Emma Watson",
-      date: 27,
-    },
-    {
-      key: "3",
-      name: "Watson",
-      date: 21,
     },
   ];
 
@@ -199,13 +107,13 @@ const App = () => {
   const handleApprove = (record) => {
     console.log("승인:", record);
 
-    toast.success(`승인되었습니다: ${record.name}`);
+    toast.success(`승인되었습니다: ${record.userName}`);
   };
 
   const handleReject = (record) => {
     console.log("취소:", record);
 
-    toast.error(`취소되었습니다: ${record.name}`);
+    toast.error(`취소되었습니다: ${record.userName}`);
   };
 
   return (
@@ -225,18 +133,104 @@ const App = () => {
       />
 
       <Container>
-        <TableWrapper>
+        <LeaveTableWrapper>
           <TableHeader>연차 신청 현황</TableHeader>
-          <Table dataSource={adjustedDataSource1} columns={columns1} />
-        </TableWrapper>
+          <Table dataSource={adjustedDataSource1} columns={columns1} rowKey="id" />
+        </LeaveTableWrapper>
 
-        <TableWrapper>
+        <DutyTableWrapper>
           <TableHeader>당직 신청 현황</TableHeader>
-          <Table dataSource={adjustedDataSource2} columns={columns2} />
-        </TableWrapper>
+          <Table dataSource={adjustedDataSource2} columns={columns2} rowKey="id"/>
+        </DutyTableWrapper>
       </Container>
     </PageContainer>
   );
 };
 
 export default App;
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+`;
+
+const PageLogoText = styled.h1`
+  font-size: 32px;
+  font-weight: bold;
+  margin-bottom: 50px;
+  margin-top: 70px;
+  margin-right: 800px;
+`;
+
+const Container = styled.div`
+  width: 70%;
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+  margin-top: 60px;
+`;
+
+const LeaveTableWrapper = styled.div`
+  border: 1px solid  ${theme.colors.green.main}; 
+  border-radius: 4px;
+  width: 600px;
+  padding: 30px;
+  border-radius: 10px;
+`;
+
+const DutyTableWrapper = styled.div`
+  border: 1px solid ${theme.colors.orange.main};
+  border-radius: 4px;
+  width: 600px;
+  padding: 30px;
+  border-radius: 10px;
+`;
+
+const TableHeader = styled.h2`
+  margin-bottom: 20px;
+  margin-right: 280px;
+`;
+
+const StyledLeaveApproveButton = styled(Button)`
+  background-color: ${theme.colors.white};
+  color: ${theme.colors.green.main};
+  margin-right: 10px;
+
+  &:hover {
+    background-color: ${theme.colors.green.main};
+    color: ${theme.colors.white};
+  }
+`;
+
+const StyledholidayRejectButton = styled(Button)`
+  background-color: ${theme.colors.green.main};
+  color: ${theme.colors.white};
+
+  &:hover {
+    background-color: ${theme.colors.green.dark};
+    color: ${theme.colors.white};
+  }
+`;
+
+const StyleddutyApproveButton = styled(Button)`
+  background-color: ${theme.colors.white};
+  color: ${theme.colors.orange.main};
+  margin-right: 10px;
+
+  &:hover {
+    background-color: ${theme.colors.orange.main};
+    color: ${theme.colors.white};
+  }
+`;
+
+const StyleddutyRejectButton = styled(Button)`
+  background-color: ${theme.colors.orange.main};
+  color: ${theme.colors.white};
+
+  &:hover {
+    background-color: ${theme.colors.orange.dark};
+    color: ${theme.colors.white};
+  }
+`;
